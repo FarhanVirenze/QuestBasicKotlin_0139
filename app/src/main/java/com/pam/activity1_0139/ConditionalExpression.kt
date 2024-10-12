043c1151fadc44045f805acdc29937791be67e55
farhan <farhanvirenze18@gmail.com>
2024-10-12 19:04:11 +0700

    Commit ConditionalExpression.kt

--- app/src/main/java/com/pam/activity1_0139/ConditionalExpression.kt
+++ app/src/main/java/com/pam/activity1_0139/ConditionalExpression.kt
@@ -1,2 +1,64 @@
 package com.pam.activity1_0139
 
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai bersarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+// If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+// When
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+
+// Try-Catch
+    val value ="10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+}
+
+fun Perulangan() {
+    println()
+    print("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y<= 5)
+}
+
+fun main() {
+    ConditionalStatement()
+    Perulangan()
+}
