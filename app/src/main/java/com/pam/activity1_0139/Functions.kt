9a5d374eba70839412f2c721177d89ddd8be11a8
farhan <farhanvirenze18@gmail.com>
2024-10-12 19:13:15 +0700

    commit Functions.kt

--- app/src/main/java/com/pam/activity1_0139/Functions.kt
+++ app/src/main/java/com/pam/activity1_0139/Functions.kt
@@ -1,1 +1,43 @@
 package com.pam.activity1_0139
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+// named argument
+// named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+// default parameter value
+// default parameter value merupakan cara untuk Memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = " Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+// Print the result if you want
+// Return the result
+fun withReturn(panjang: Int, lebar: Int): Int {
+    val hasil = panjang * lebar
+    println(hasil)
+    return hasil
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
