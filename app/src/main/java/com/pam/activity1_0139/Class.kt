033b341a72b675ec58346c5477d1f22fc1c8f170
farhan <farhanvirenze18@gmail.com>
2024-10-12 19:38:07 +0700

    commit new Class.kt

--- app/src/main/java/com/pam/activity1_0139/Class.kt
+++ app/src/main/java/com/pam/activity1_0139/Class.kt
@@ -5,14 +5,14 @@ class Motor()
 class Contact(val id: Int, var email: String)
 
 fun main() {
-    val contact = Contact(1, "mary@gmail.com")
+val contact = Contact(1, "mary@gmail.com")
 
-    // Prints the value of the property: email
-    println(contact.email)
+// Prints the value of the property: email
+println(contact.email)
 
-    // Updates the value of the property: email
-    contact.email = "jane@gmail.com"
+// Updates the value of the property: email
+contact.email = "jane@gmail.com"
 
-    // Prints the new value of the property: email
-    println(contact.email)
+// Prints the new value of the property: email
+println(contact.email)
 }
