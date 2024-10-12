78842f2f25c3b715aa2448ee0655b0d44a49c039
farhan <farhanvirenze18@gmail.com>
2024-10-12 19:14:16 +0700

    commit LambdaFunction.kt

--- app/src/main/java/com/pam/activity1_0139/LambdaFunction.kt
+++ app/src/main/java/com/pam/activity1_0139/LambdaFunction.kt
@@ -1,2 +1,17 @@
 package com.pam.activity1_0139
 
+// Lambda Functions
+// Lambda Functions adalah fungsi yang tidak memiliki nama
+// Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi.
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+// Dapat ditulis dalam eskpresi lambda sbb :
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+// Hello
+}
